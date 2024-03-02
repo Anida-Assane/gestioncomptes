308ee93c173ae361ed85e6f2088103fa6f8450a6
Anida Assane Yacouba <aniyacoub18@gmail.com>
2024-03-02 14:24:25 +0000

    ajout dockerfile

--- src/main/java/org/sid/compteservice/CompteServiceApplication.java
+++ src/main/java/org/sid/compteservice/CompteServiceApplication.java
@@ -2,10 +2,16 @@ package org.sid.compteservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CompteServiceApplication {
-
+    @RequestMapping("/")
+    public String home(){
+        return "hello world";
+    }
     public static void main(String[] args) {
         SpringApplication.run(CompteServiceApplication.class, args);
     }
